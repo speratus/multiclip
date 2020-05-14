6bfed50c374df372e0b60be16fe7ccb6e118062a
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-05-14 10:03:27 -0400

    Sets up channels plugin

--- multiclip/settings.py
+++ multiclip/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'channels'
 ]
 
 MIDDLEWARE = [
@@ -69,6 +70,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'multiclip.wsgi.application'
 
+
+ASGI_APPLICATION = 'multiclip.routing.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
