9bd6737f8a2887cfa789c99c77304f9c9e7b1889
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-06-22 14:35:09 -0400

    finishes login view

--- clipper/views.py
+++ clipper/views.py
@@ -4,12 +4,21 @@ from django.contrib.auth import authenticate, login
 
 from .forms import LoginForm
 
+
 def main(request):
     render(request, 'main.html')
 
+
 def login(request):
     if request.method == 'POST':
-        pass
+        login_form = LoginForm(request.POST)
+        if login_form.is_valid():
+            data = login_form.cleaned_data
+            user = authenticate(username=data['username'], password=data['password'])
+
+            if user is not None:
+                login(request, user)
+                redirect(reverse('main'))
     else:
         loginform = LoginForm()
         render(request, 'login.html', {'form': loginform})
