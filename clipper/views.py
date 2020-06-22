c2ce542c54f329a152471360e3c729e205ea2a2a
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-06-22 14:04:56 -0400

    adds login view

--- clipper/views.py
+++ clipper/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.contrib.auth import authenticate, login
 
 from .forms import LoginForm
+
+def main(request):
+    render(request, 'main.html')
 
 def login(request):
     if request.method == 'POST':
