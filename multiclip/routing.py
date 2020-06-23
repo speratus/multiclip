ffbd89ad31339f3c54dd396a8a9f71d7aadc589e
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-06-23 11:35:01 -0400

    sets up websocket routings

--- multiclip/routing.py
+++ multiclip/routing.py
@@ -1,5 +1,11 @@
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import clipper.routing
 
 application = ProtocolTypeRouter({
-
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            clipper.routing.websocket_urlpatterns
+        )
+    )
 })
