49e9d3071dcfb565ba48979b58ca46c858c24678
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-06-23 11:45:39 -0400

    fixing websocket routing bug

--- clipper/routing.py
+++ clipper/routing.py
@@ -3,5 +3,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/clip/(?P<room-id>\w+)/$', consumers.WebsocketConsumer)
+    re_path(r'ws/clipboard/(?P<room_id>\w+)/$', consumers.WebsocketConsumer)
 ]
