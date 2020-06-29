de5665dc7164b8504abf48dbbdf82b7da2f30155
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-06-29 14:30:07 -0400

    adds new signup form

--- clipper/forms.py
+++ clipper/forms.py
@@ -3,3 +3,9 @@ from django import forms
 class LoginForm(forms.Form):
     username = forms.CharField(label="Username", max_length=256)
     password = forms.CharField(label="Password", widget=forms.PasswordInput, max_length=256)
+
+class SignupForm(forms.Form):
+    email = forms.CharField(label="Email", max_length=256)
+    username = forms.CharField(label="Username", max_length=256)
+    password = forms.CharField(label="Password", max_length=256, widget=forms.PasswordInput)
+    confirm_password = forms.CharField(label="Confirm Password", max_length=256, widget=forms.PasswordInput)
