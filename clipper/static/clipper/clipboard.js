2e2b23b883fcd086c56a508dce49f7de9a14af67
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-06-29 17:27:46 -0400

    refactors clipboard.js to make data sendable

--- clipper/static/clipper/clipboard.js
+++ clipper/static/clipper/clipboard.js
@@ -3,19 +3,51 @@ function getCookieValue(name) {
     return value;
 }
 
-const roomId = getCookieValue('clipboard-id');
+function createButton(parentId, text, clickFunction) {
+    const button = document.createElement('button');
+    button.textContent = text;
+    button.onclick = clickFunction;
+    const parent = document.getElementById(parentId);
+    parent.appendChild(button);
+}
+
+const clipboardId = getCookieValue('clipboard-id');
 
 const clipSocket = new WebSocket(
     'ws://'
     + window.location.host
     + '/ws/clipboard/'
-    + roomId
+    + clipboardId
     + '/'
 );
 
+function sendClipboard() {
+    console.log('sending text');
+    navigator.clipboard.readText().then(clipText => {
+        console.log('read clipboard.');
+        console.log('what is Clipsocket?', typeof clipSocket);
+        clipSocket.send(JSON.stringify({
+            clipboard: clipText
+        }));
+    });
+}
+
+function receiveClipboard(clipboardText) {
+    navigator.clipboard.writeText(clipboardText).then(() => {
+        const local = document.querySelector("#local-clipboard");
+        local.textContent = clipboardText;
+    });
+}
+
 clipSocket.onmessage = function(e) {
     const data = JSON.parse(e.data);
+    console.log('received data', data)
     document.querySelector('#cloud-clipboard').textContent = data.clipboard;
+    createButton('cloud-container', 'Paste from Cloud', () => {receiveClipboard(data.clipboard)});
+}
+
+clipSocket.onerror = function(e) {
+    console.log('encountered error:', e)
 }
 
 clipSocket.onclose = function(e) {
@@ -34,4 +66,7 @@ navigator.clipboard.readText().then(clipText => {
     console.log('text is undefined?', clipText === undefined);
     console.log(clipText);
     document.querySelector('#local-clipboard').textContent = clipText;
+    if (clipText !== "") {
+        createButton('local-container', 'Copy to Cloud', sendClipboard);
+    }
 });
