feff6b6fc062a3025d9bcd5f4c627ce52d6d66f6
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-07-15 15:35:03 -0400

    adapts sign up page for new styles

--- clipper/forms.py
+++ clipper/forms.py
@@ -23,7 +23,43 @@ class LoginForm(forms.Form):
     )
 
 class SignupForm(forms.Form):
-    email = forms.CharField(label="Email", max_length=256)
-    username = forms.CharField(label="Username", max_length=256)
-    password = forms.CharField(label="Password", max_length=256, widget=forms.PasswordInput)
-    confirm_password = forms.CharField(label="Confirm Password", max_length=256, widget=forms.PasswordInput)
+    email = forms.CharField(
+        label="Email",
+        max_length=256,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'input',
+                'placeholder': 'Email'
+            }
+        )
+    )
+    username = forms.CharField(
+        label="Username",
+        max_length=256,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'input',
+                'placeholder': 'Username'
+            }
+        )
+    )
+    password = forms.CharField(
+        label="Password",
+        max_length=256,
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'input',
+                'placeholder': 'Password'
+            }
+        )
+    )
+    confirm_password = forms.CharField(
+        label="Confirm Password",
+        max_length=256,
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'input',
+                'placeholder': 'Confirm Password'
+            }
+        )
+    )
