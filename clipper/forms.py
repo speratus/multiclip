ef5222c4456158dd69411cbee3bf5aefad91a9a1
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-07-15 15:15:56 -0400

    adapts login page for new styles

--- clipper/forms.py
+++ clipper/forms.py
@@ -1,8 +1,26 @@
 from django import forms
 
 class LoginForm(forms.Form):
-    username = forms.CharField(label="Username", max_length=256)
-    password = forms.CharField(label="Password", widget=forms.PasswordInput, max_length=256)
+    username = forms.CharField(
+        label="Username",
+        max_length=256,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'input',
+                'placeholder': 'Username'
+            }
+        )
+    )
+    password = forms.CharField(
+        label="Password",
+        max_length=256,
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'input',
+                'placeholder': 'Password'
+            }
+        )
+    )
 
 class SignupForm(forms.Form):
     email = forms.CharField(label="Email", max_length=256)
