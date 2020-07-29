76dbe43c65b4270c900b0812d9fcbb8042b12460
Andrew Luchuk <spaxumof@gmail.com>
2020-07-29 12:03:32 -0400

    adds an index page.

--- clipper/urls.py
+++ clipper/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
 from . import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
     path('clipboard/', views.main, name='main'),
-    path('logout', views.signout, name='logout')
+    path('logout/', views.signout, name='logout'),
+    path('', TemplateView.as_view(template_name='clipper/index.html'), name='index')
 ]
